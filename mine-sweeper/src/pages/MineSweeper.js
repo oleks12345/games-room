91cfdae68036ffa020e21d736b3e8f002226a6a2
Aleksander Uniatowicz <oleks0709@gmail.com>
2019-06-04 19:10:53 +0200

    fix MineSweeper styles

--- mine-sweeper/src/pages/MineSweeper.js
+++ mine-sweeper/src/pages/MineSweeper.js
@@ -4,8 +4,8 @@ import PageTemplate from 'templates/PageTemplate/PageTemplate';
 import GameBoard from 'components/GameBoard/GameBoard';
 
 const GameWrapper = styled.div`
-   height: 100vh;
-   width: 100%;
+   min-height: 100vh;
+   min-width: 100%;
    display: flex;
    padding: 25px;
    justify-content: center;
