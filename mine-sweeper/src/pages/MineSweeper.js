2f9bfee5191d2d5a2f77feaf135c2b71ced94add
Aleksander Uniatowicz <oleks0709@gmail.com>
2019-06-04 16:24:42 +0200

    add GameBoard

--- mine-sweeper/src/pages/MineSweeper.js
+++ mine-sweeper/src/pages/MineSweeper.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
 import PageTemplate from 'templates/PageTemplate/PageTemplate';
+import GameBoard from 'components/GameBoard/GameBoard';
 
 const GameWrapper = styled.div`
    height: 100vh;
    width: 100%;
    display: flex;
+   padding: 25px;
    justify-content: center;
    align-items: center;
 `;
@@ -14,7 +16,7 @@ const MineSweeper = () => {
    return (
       <PageTemplate>
          <GameWrapper>
-            <h1>hello</h1>
+            <GameBoard />
          </GameWrapper>
       </PageTemplate>
    );
