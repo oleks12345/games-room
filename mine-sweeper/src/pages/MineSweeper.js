6b5b843bb6575074c2f7b2108c8d75ae58f5cc08
Aleksander Uniatowicz <oleks0709@gmail.com>
2019-06-04 14:29:54 +0200

    create GameWrapper

--- mine-sweeper/src/pages/MineSweeper.js
+++ mine-sweeper/src/pages/MineSweeper.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
+import PageTemplate from 'templates/PageTemplate/PageTemplate';
+
+const GameWrapper = styled.div`
+   height: 100vh;
+   width: 100%;
+   display: flex;
+   justify-content: center;
+   align-items: center;
+`;
 
 const MineSweeper = () => {
    return (
-      <div>
-         <h1>hello</h1>
-      </div>
+      <PageTemplate>
+         <GameWrapper>
+            <h1>hello</h1>
+         </GameWrapper>
+      </PageTemplate>
    );
 };
 
