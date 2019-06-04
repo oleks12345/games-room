5081ac4b282259d52e99042365e86a494bd21429
Aleksander Uniatowicz <oleks0709@gmail.com>
2019-06-04 19:39:16 +0200

    add Cell handlers

--- mine-sweeper/src/components/Cell/Cell.js
+++ mine-sweeper/src/components/Cell/Cell.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 import bombIcon from 'assets/images/mine.png';
+import flagIcon from 'assets/images/flag.svg';
 
 const StyledCell = styled.button`
    border-width: 10px;
@@ -19,12 +20,14 @@ const EmptyCell = styled.div`
    background-color: #dddddd;
 `;
 const CellImg = styled.img`
+   background-color: #dddddd;
    width: 100%;
    height: 100%;
 `;
 
 const Cell = ( { cellData, click } ) => {
-   const { isOpen, type, row, column } = cellData;
+   const { isOpen, isFlagged, type, row, column } = cellData;
+
    return (
       <>
          {isOpen ? (
@@ -34,7 +37,17 @@ const Cell = ( { cellData, click } ) => {
                <EmptyCell />
             )
          ) : (
-            <StyledCell onClick={ () => click( row, column ) } />
+            <StyledCell
+               onContextMenu={ ( e ) => {
+                  e.preventDefault();
+                  click( 'right', row, column );
+               } }
+               onClick={ () => {
+                  click( 'left', row, column );
+               } }
+            >
+               {isFlagged && <CellImg src={ flagIcon } />}
+            </StyledCell>
          )}
       </>
    );
