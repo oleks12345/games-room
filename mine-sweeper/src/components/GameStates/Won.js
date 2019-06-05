303cbb3d4bdb83a6282cbcd033ca9ccbec84c723
Aleksander Uniatowicz <oleks0709@gmail.com>
2019-06-05 12:03:25 +0200

    create Won compontent

---
+++ mine-sweeper/src/components/GameStates/Won.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const WonWrapper = styled.div`
+   /* ... */
+`;
+
+const Won = () => {
+   return (
+      <WonWrapper>
+         <div> You won </div>
+      </WonWrapper>
+   );
+};
+
+export default Won;
