7ec8646e53b5d73b4c8043d39988ac6eae0416ed
Aleksander Uniatowicz <oleks0709@gmail.com>
2019-06-05 17:32:49 +0200

    add button animation

--- mine-sweeper/src/components/StartMenu/StartMenu.js
+++ mine-sweeper/src/components/StartMenu/StartMenu.js
@@ -7,14 +7,48 @@ const StyledWrapper = styled.div`
    justify-content: center;
    align-items: center;
    flex-direction: column;
+   background-color: #ddd;
+   padding: 40px;
 `;
 
 const StyledButton = styled.button`
+   position: relative;
+   overflow: hidden;
    font-size: 50px;
    font-weight: bold;
    padding: 10px 50px;
    background-color: black;
    color: white;
+   border: 10px solid black;
+   ::before,
+   ::after {
+      content: '';
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+   }
+   ::before {
+      background-color: red;
+      width: 100%;
+      height: 100%;
+      opacity: 0;
+      transition: transform 0.5s linear, opacity 0.5s linear;
+      transform: translate(-50%, 200px);
+   }
+   :hover::before {
+      opacity: 1;
+      transform: translate(-50%, -50%);
+   }
+   ::after {
+      background-color: black;
+      width: calc(100% - 20px);
+      height: calc(100% - 20px);
+   }
+`;
+const SpanTop = styled.span`
+   position: relative;
+   z-index: 10;
 `;
 const StyledTitle = styled.h1`
    font-size: 75px;
@@ -24,7 +58,9 @@ const StartMenu = ( { start } ) => {
    return (
       <StyledWrapper>
          <StyledTitle>MineSweeper</StyledTitle>
-         <StyledButton onClick={ start }>Start</StyledButton>
+         <StyledButton onClick={ start }>
+            <SpanTop>Start</SpanTop>
+         </StyledButton>
       </StyledWrapper>
    );
 };
