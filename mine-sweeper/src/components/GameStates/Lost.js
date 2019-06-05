42c52fd7fd26b4de808755cb6a665b0d4e248117
Aleksander Uniatowicz <oleks0709@gmail.com>
2019-06-05 12:02:56 +0200

    add Lost component

---
+++ mine-sweeper/src/components/GameStates/Lost.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const LostWrapper = styled.div`
+   /* ... */
+`;
+
+const Lost = () => {
+   return (
+      <LostWrapper>
+         <div> You lost </div>
+      </LostWrapper>
+   );
+};
+
+export default Lost;
