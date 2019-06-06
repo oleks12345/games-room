1ca4ac5bf131f92e4ecb90ca3d998545b7c04137
Aleksander Uniatowicz <oleks0709@gmail.com>
2019-06-06 13:27:55 +0200

    change file name

--- mine-sweeper/src/components/StartMenu/Menu/styles.js
+++ mine-sweeper/src/components/StartMenu/Menu/styles.js
@@ -1,22 +1,5 @@
 import styled from 'styled-components';
 
-export const StyledWrapper = styled.div`
-   display: flex;
-   justify-content: center;
-   align-items: center;
-   flex-direction: column;
-   background-color: #ddd;
-   border-radius: 5em;
-   font-size: 5px;
-   padding: 4em;
-   @media (min-width: 640px) {
-      font-size: 8px;
-   }
-   @media (min-width: 768px) {
-      font-size: 10px;
-   }
-`;
-
 export const StartButton = styled.button`
    position: relative;
    overflow: hidden;
