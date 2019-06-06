a562c3ea784c47cd9a0bf20e92e06e2ba6c6fe20
Aleksander Uniatowicz <oleks0709@gmail.com>
2019-06-06 14:18:54 +0200

    create Settings components

--- mine-sweeper/src/components/StartMenu/Settings/Settings.js
+++ mine-sweeper/src/components/StartMenu/Settings/Settings.js
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-const Settings = ( { toogleSettingsMenu } ) => {
+const Settings = ( { toogleSettingsMenu, settings, changeSettings } ) => {
+   const [ inputs, setInputs ] = useState( settings );
+   const handleInputChange = ( e ) => {
+      setInputs( {
+         ...inputs,
+         [e.target.name]: parseInt( e.target.value ),
+      } );
+   };
    return (
-      <div>
+      <>
+         {Object.keys( settings ).map( ( setting ) => (
+            <div key={ 'input=' + setting }>
+               <label htmlFor={ setting }>
+                  {setting}:
+                  <input
+                     value={ inputs[setting] }
+                     onChange={ handleInputChange }
+                     name={ setting }
+                  />
+               </label>
+            </div>
+         ) )}
+         <button onClick={ () => changeSettings( inputs ) } type="button">
+            Confirm Settings
+         </button>
          <button onClick={ toogleSettingsMenu } type="button">
             Go back
          </button>
-      </div>
+      </>
    );
 };
 Settings.propTypes = {
-   toogleSettingsMenu: PropTypes.bool.isRequired,
+   toogleSettingsMenu: PropTypes.func.isRequired,
+   settings: PropTypes.shape().isRequired,
+   changeSettings: PropTypes.func.isRequired,
 };
 export default Settings;
