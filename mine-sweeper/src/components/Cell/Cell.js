0e5fb1d036617e4bf3480f96779cb0b59664673f
Aleksander Uniatowicz <oleks0709@gmail.com>
2019-06-07 11:18:39 +0200

    move cellColor select function to separate file

--- mine-sweeper/src/components/Cell/Cell.js
+++ mine-sweeper/src/components/Cell/Cell.js
@@ -5,53 +5,25 @@ import bombIcon from 'assets/images/mine.png';
 import flagIcon from 'assets/images/flag.svg';
 
 import { StyledCell, EmptyCell, CellImg } from './Cell-styles';
+import { cellColor } from './cellColor';
 
 const Cell = ( { cellData, click } ) => {
    const { isOpen, isFlagged, number, type, row, column, color } = cellData;
-   let cellColor = '';
-   switch ( number ) {
-   case 1:
-      cellColor = 'blue';
-      break;
-   case 2:
-      cellColor = 'green';
-      break;
-   case 3:
-      cellColor = 'red';
-      break;
-   case 4:
-      cellColor = 'purple';
-      break;
-   case 5:
-      cellColor = 'maroon';
-      break;
-   case 6:
-      cellColor = 'turquoise';
-      break;
-   case 7:
-      cellColor = 'black';
-      break;
-   case 8:
-      cellColor = 'grey';
-      break;
-   default:
-      break;
-   }
+
    return (
       <>
          {isOpen ? (
             type === 'bomb' ? (
                <CellImg color={ color } src={ bombIcon } />
             ) : (
-               <EmptyCell style={ { color: cellColor } }>
+               <EmptyCell style={ { color: cellColor( number ) } }>
                   {number !== 0 && number}
                </EmptyCell>
             )
          ) : (
             <StyledCell
                color={ color }
-               onContextMenu={ ( e ) => {
-                  e.preventDefault();
+               onContextMenu={ () => {
                   click( 'right', row, column );
                } }
                onClick={ () => {
---
+++ mine-sweeper/src/components/Cell/cellColor.js
@@ -0,0 +1,22 @@
+export const cellColor = ( number ) => {
+   switch ( number ) {
+   case 1:
+      return 'blue';
+   case 2:
+      return 'green';
+   case 3:
+      return 'red';
+   case 4:
+      return 'purple';
+   case 5:
+      return 'maroon';
+   case 6:
+      return 'turquoise';
+   case 7:
+      return 'black';
+   case 8:
+      return 'grey';
+   default:
+      return 'grey';
+   }
+};
